7acc3391572341f5496bba0b733160bb76259777
AeroPitch <azharadityapratama48@gmail.com>
2020-05-05 16:55:10 +0700

    some changes

--- pemrograman-dasar/10.E. Komposisi Fungsi.cpp
+++ pemrograman-dasar/10.E. Komposisi Fungsi.cpp
@@ -1,6 +1,15 @@
 #include <cstdio>
-#include <cmath>
+// fungsi abs() juga dapat diakses
+// menggunakan STL <cmath>
 using namespace std;
+
+int abs(int x) {
+    if (x < 0) {
+        return x * -1;
+    } else {
+        return x;
+    }
+}
 
 void func(int A, int B, int &x) {
     x = abs(A * x + B);
--- pemrograman-dasar/10.F. Teman Dekat.cpp
+++ pemrograman-dasar/10.F. Teman Dekat.cpp
@@ -1,18 +1,37 @@
 #include <cstdio>
-#include <cmath>
+// fungsi abs() dan pow() juga dapat diakses
+// menggunakan STL <cmath>
 using namespace std;
+
+int abs(int x) {
+    if (x < 0) {
+        return x * -1;
+    } else {
+        return x;
+    }
+}
+
+int pow(int b, int n) {
+    int x = b;
+    for (int i = 2; i <= n; i++) {
+        x *= b;
+    }
+    return x;
+}
 
 int func(int Xi, int Yi, int Xj, int Yj, int D) {
     return pow(abs(Xj - Xi), D) + pow(abs(Yj - Yi), D);
 }
 
 int main() {
-    int N, D, min = 2000000, max = -1, temp;
+    int N, D, min, max, temp;
     scanf("%d %d", &N, &D);
     int X[N], Y[N];
     for (int i = 0; i < N; i++) {
         scanf("%d %d", &X[i], &Y[i]);
     }
+    min = func(X[0], Y[0], X[1], Y[1], D);
+    max = min;
     for (int i = 0; i < N; i++) {
         for (int j = i + 1; j < N; j++) {
             temp = func(X[i], Y[i], X[j], Y[j], D);
